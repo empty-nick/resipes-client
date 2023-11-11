708ae7e8789171a3aa6efb3bb346e82026d94e81
empty-nick <leard246@gmail.com>
2023-11-11 14:16:41 +0300

    feat: add menu item without customization

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import styles from './Header.module.css'
+import { Menu } from "../Menu";
 
 export const Header = () => {
+  const mokeMenu = [
+    {name: 'Home', url: '/', id: 'ho,eo'},
+    {name: 'News', url: '/news', id: 'cjdnskcj'},
+    {name: 'Recipes', url: '/recipes', id: 'knkvnf'}
+  ]
   return (
     <div className={styles.content}>
-      <div className={styles.menu}>Some menu</div>
+      <div className={styles.menu}>
+        {mokeMenu.map( ({name, url, id}) => <Menu name={name} url={url} key={id} />)}
+      </div>
       <div className={styles.profile}>Profile</div>
     </div>
   )
