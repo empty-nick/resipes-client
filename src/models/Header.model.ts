f8c78cc9fb9b43be9dc19ed0ead70d4f7d130c28
empty-nick <leard246@gmail.com>
2023-11-18 23:39:33 +0300

    fix: fixed imports, menu api

--- src/models/Header.model.ts
+++ src/models/Header.model.ts
@@ -3,3 +3,10 @@ export interface IHeaderItems {
   url: string;
   id: string | number;
 }
+
+export const headerItems: IHeaderItems[] = [
+  { name: 'Home', url: '/', id: 1 },
+  { name: 'News', url: '/news', id: 2 },
+  { name: 'Recipes', url: '/recipes', id: 3 },
+  { name: 'About project', url: '/about', id: 4 }
+]
