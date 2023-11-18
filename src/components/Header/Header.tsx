1ba0001fc8192eb3f9e6b7988e998511475925fe
empty-nick <leard246@gmail.com>
2023-11-18 13:52:41 +0300

    feat: fixed header and menu items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import styles from './Header.module.css'
 import { HeaderItem } from "../HeaderItem";
 import { Img } from "react-image";
 import { IHeaderItems } from "../../models/Header.model.ts";
+import { FaUser } from "react-icons/fa";
 
 export const Header = () => {
   const mokeMenu: IHeaderItems[] = [
@@ -11,16 +12,24 @@ export const Header = () => {
     {name: 'About project', url: '/about', id: 'knkvnf'}
   ]
   return (
-    <div className={styles.content}>
-      <div className={styles.logo}>
-        <Img src={''} />
-      </div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <p>logo</p>
+          <Img src={'../../assets/logo.jpg'} />
+        </div>
 
-      <div className={styles.menu}>
-        {mokeMenu.map( ({name, url, id}) => <HeaderItem name={name} url={url} key={id} />)}
-      </div>
+        <div className={styles.menu}>
+          {mokeMenu.map( ({name, url, id}) => <HeaderItem name={name} url={url} key={id} />)}
+        </div>
 
-      <div className={styles.profile}>Profile</div>
+        <div className={styles.profile}>
+          <FaUser />
+          <span className={styles.usernameField}>
+            Username
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
