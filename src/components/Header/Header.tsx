a5640f0fb4192a8aa166a959d08e0bc33a43dfd1
empty-nick <85650519+empty-nick@users.noreply.github.com>
2023-11-18 12:40:42 -0800

    fix: fixed imports, menu api (#4)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,11 @@
-import { useGetHeaderItemsQuery } from "../../store/serve/serve.api.ts";
 import { Img } from "react-image";
 import { FaUser } from "react-icons/fa";
 import { HeaderItem } from "../HeaderItem";
-import { IHeaderItems } from "../../models/Header.model.ts";
+import { headerItems } from "../../models/Header.model.ts";
 import styles from './Header.module.css'
 
 export const Header = () => {
-  const { data, isError, isLoading } = useGetHeaderItemsQuery(null)
-  console.log(isError, data)
 
-  const mokeMenu: IHeaderItems[] = [
-    { name: 'Home', url: '/', id: 'ho,eo' },
-    { name: 'News', url: '/news', id: 'cjdnskcj' },
-    { name: 'Recipes', url: '/recipes', id: 'knkvnf' },
-    { name: 'About project', url: '/about', id: 'kfvdklkk' }
-  ]
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -24,14 +15,9 @@ export const Header = () => {
         </div>
 
         <div className={styles.menu}>
-          {(isError || isLoading) ?
-            mokeMenu.map(({ name, url, id }) => (
-            <HeaderItem name={name} url={url} key={id}/>
-          )) : (
-            data?.map(item => (
-              <HeaderItem name={item.name} url={item.url} key={item.id}/>
-            ))
-          )}
+          {headerItems.map( item => (
+            <HeaderItem name={item.name} url={item.url} key={item.id} />
+          ))}
         </div>
 
         <div className={styles.profile}>
--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,4 +1,4 @@
-import {useSelector} from "react-redux";
-import {TStore} from "../store/store.ts";
+import { useSelector } from "react-redux";
+import { TStore } from "../store/store.ts";
 
 export const useAppSelector: TStore = useSelector
