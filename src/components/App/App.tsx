fd89f7ad6f9a3687f6f30216be90f871e17646eb
empty-nick <leard246@gmail.com>
2023-11-19 00:32:53 +0300

    feat: add logic to contain recipes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import {ReactElement} from "react";
 import {useRoutes} from "react-router-dom";
 import {applicationRoutes} from "../../pages/routes.tsx";
+import { Header } from "../Header";
 
 function App(): ReactElement {
   const routes = useRoutes(applicationRoutes)
   return (
     <>
+      <Header />
       {routes}
     </>
   )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
-import { Header } from "../../components/Header";
 import { Welcome } from "../../components/Welcome";
 import styles from './Home.module.css'
 
 export const Home = () => {
   return (
     <div className={styles.content}>
-      <Header />
       <Welcome />
     </div>
   )
